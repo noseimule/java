7a064a650013b7267becbf9ae5ff3488973b704d
noseimule <90968282+noseimule@users.noreply.github.com>
2022-02-22 19:43:50 +0300

    Add files via upload

---
+++ 1 practika/qr.java
@@ -0,0 +1,10 @@
+public class qr {
+    public static void main(String[] args) {
+        if (args.length == 2) {
+            System.out.print("\t"+ args[0] +" + "+ args[1] +" = "+ (Integer.parseInt(args[0])+Integer.parseInt(args[1])));
+        }
+        else {
+            System.out.print("\tНеверное количество параметров.");
+        }
+    }
+}
